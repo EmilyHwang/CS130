d4dd2aac6e2709f8378f9bed3f308d8272b94124
Sharon Hsieh <sharonhsieh@ucla.edu>
2016-05-04 23:01:37 -0700

    home page now shows basic tweet info from random influencers

--- beehive/app/beehive.py
+++ beehive/app/beehive.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import twitter
+import rand_influencers
 
 # Configurations
 DEBUG = True
@@ -17,32 +18,33 @@ app.config.from_object(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    # 'data' is a variable that passes info to template for rendering
-    # So for example, place stuff we retrieve from the database, api call, etc. into data
-    data = {}
-    return render_template('index.html', data=data)
+	# 'data' is a variable that passes info to template for rendering
+	# So for example, place stuff we retrieve from the database, api call, etc. into data
+	#data = {}
+	users = rand_influencers.get_users(9)
+	return render_template('index.html', users=users)
 
-@app.route('/search', methods=['POST'])
+@app.route('/search', methods=['POST', 'GET'])
 def search():
 	if request.method == 'POST':
 		searchword = request.form['searchterm']
 		potential_influencers = twitter.search_twitter(searchword)
 		return render_template('search_result.html', potential_influencers=potential_influencers)
-	else: 
+	else:
 		data = {}
 		return render_template('search.html', data=data)
 
 @app.route('/about')
 def about():
-    data = {}
-    return render_template('about.html', data=data)
+	data = {}
+	return render_template('about.html', data=data)
 
 
 @app.route('/contact')
 def contact():
-    data = {}
-    return render_template('contact.html', data=data)
+	data = {}
+	return render_template('contact.html', data=data)
 
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
