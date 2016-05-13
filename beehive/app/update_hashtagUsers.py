1f8c3943a556f8dd6b43b99e5b5af08a31b094ef
Irene Yeh <pigrabbit87@yahoo.com.tw>
2016-05-13 16:51:50 -0700

    finish update_hashtag

--- beehive/app/update_hashtagUsers.py
+++ beehive/app/update_hashtagUsers.py
@@ -7,52 +7,27 @@
 import os, sys, MySQLdb, datetime, time
 from cass_orm import Cassandra
 from mysql_orm import MySQL
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from twitter_search import Search
 
 class HashtagUsers:
 	def __init__(self):
-		self.driver = webdriver.Chrome(executable_path=r"/Users/IreneY/Library/chromedriver")
-		today = datetime.datetime.now()
-		self.end_date = "%s-%s-%s" % (today.year, today.month, today.day)
-		weekAgo = today - datetime.timedelta(days=7)
-		self.start_date = "%s-%s-%s" % (weekAgo.year, weekAgo.month, weekAgo.day)
+		self.mysql = MySQL("beehive")
 
 	def update(self):
-		mysql = MySQL("beehive")
-		cass = Cassandra("beehive")
-		totalValidResults = 0
-
 		# Get all the hashtags from MySQL table
-		hashtagSets = mysql.getHashtags()
+		hashtagSets = self.mysql.getHashtags()
 		# For each hashtag
 		for hashtag in hashtagSets:
 			hashtag = hashtag[1:]
-			#	Find the list of users for this hashtag and put it in a dictionary, D
-			users_temp = cass.get_user_from_hashtag(hashtag)
-			users = set()
-			for user in users_temp:		
-				users.add(user.username)
-
-			# Use selenium to crawl the hashtag search tweets results
-			query = "https://twitter.com/search?vertical=default&q=%23" + hashtag + "%20since%3A" + self.start_date + "%20until%3A" + self.end_date + "%20include%3Aretweets&src=typd"		
-			self.driver.get(query)
-			# If the tweets have more than 10 retweets, then look at the user info
-
-			#	In each results, find the user associated
-			#	If go to the user profile, get their information (follower, tweets, retweets count)				
-
-	def scroll():
-		for _ in xrange(0, 10):
-		  self.driver.execute_script("window.scrollTo(0, 10000);")
-		  time.sleep(2)
-
-	def quit(self):
-		self.driver.quit()
+			s = Search(hashtag)
+			
+			# Do new searches
+			potential_influencers = s.search_twitter_api(hashtag)
+			# Update the hashtag timestamp in MySQL
+			self.mysql.updateHashtag(hashtag)
+			# Update the Cassandra table
+			s.update_cassandra(potential_influencers)
 
 if __name__ == "__main__":
 	y = HashtagUsers()
 	y.update()
-	y.quit()
