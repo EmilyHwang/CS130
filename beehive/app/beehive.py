920399ef51c0f6c185149e5fcf4b7c2e3cfb5a63
Zoe Xi <email@zoexi.com>
2016-05-15 15:58:28 -0700

    use auth handler in twitter_search

--- beehive/app/beehive.py
+++ beehive/app/beehive.py
@@ -5,7 +5,7 @@ import twitter
 import rand_influencers
 import pdb
 import categories
-from twitter_search import Search
+import twitter_search
 
 # Configurations
 DEBUG = True
@@ -27,8 +27,8 @@ twitter_oauth = oauth.remote_app('twitter',
                                  request_token_url='https://api.twitter.com/oauth/request_token',
                                  access_token_url='https://api.twitter.com/oauth/access_token',
                                  authorize_url='https://api.twitter.com/oauth/authenticate',
-                                 consumer_key=twitter.consumer_key,
-                                 consumer_secret=twitter.consumer_secret
+                                 consumer_key=twitter_search.consumer_key,
+                                 consumer_secret=twitter_search.consumer_secret
                                  )
 
 
@@ -40,16 +40,16 @@ def get_twitter_token(token=None):
 @app.route('/')
 @app.route('/index')
 def index():
-	# 'data' is a variable that passes info to template for rendering
-	# So for example, place stuff we retrieve from the database, api call, etc. into data
-	#data = {}
+    # 'data' is a variable that passes info to template for rendering
+    # So for example, place stuff we retrieve from the database, api call, etc. into data
+    #data = {}
 
-	#test code
-	cats = categories.getAllCategories();
-	#end test code
+    #test code
+    cats = categories.getAllCategories();
+    #end test code
 
-	users = rand_influencers.get_users(3)
-	return render_template('index.html', users=users, categories=cats)
+    users = rand_influencers.get_users(3)
+    return render_template('index.html', users=users, categories=cats)
 
 
 @app.route('/search', methods=['POST'])
@@ -63,22 +63,22 @@ def search():
 
     if request.method == 'POST':
         query = request.form['user-input']
-		search = Search(query)
-		potential_influencers = search.search_twitter()
+        search = twitter_oauth.Search(query)
+        potential_influencers = search.search_twitter()
 
-		links = []
-		for name in potential_influencers:
-			links.append('https://twitter.com/' + name)
+        links = []
+        for name in potential_influencers:
+            links.append('https://twitter.com/' + name)
 
-		return render_template('search_results.html', query=query, links=links, potential_influencers=potential_influencers)
+        return render_template('search_results.html', query=query, links=links, potential_influencers=potential_influencers)
 
-	else:
-		return redirect('/search-page')
+    else:
+        return redirect('/search-page')
 
 
 @app.route('/search-page')
 def search_page():
-	return render_template('search_page.html')
+    return render_template('search_page.html')
 
 @app.route('/login')
 def login():
@@ -94,8 +94,8 @@ def oauth_authorized(response):
         return redirect(next_url)
 
     # Since auth is defined in twitter.py. Later change twitter to twitter_search
-    twitter.access_token = response['oauth_token']
-    twitter.access_token_secret = response['oauth_token_secret']
+    twitter_search.access_token = response['oauth_token']
+    twitter_search.access_token_secret = response['oauth_token_secret']
     session['screen_name'] = response['screen_name']
 
     session['twitter_token'] = (
@@ -104,16 +104,16 @@ def oauth_authorized(response):
     )
 
     # For web apps, we need to re-build the auth handler...
-    twitter.auth = OAuthHandler(twitter.consumer_key, twitter.consumer_secret)
-    twitter.auth.set_access_token(twitter.access_token, twitter.access_token_secret)
-    twitter.api = API(twitter.auth)
+    twitter_search.auth = OAuthHandler(twitter_search.consumer_key, twitter_search.consumer_secret)
+    twitter_search.auth.set_access_token(twitter_search.access_token, twitter_search.access_token_secret)
+    twitter_search.api = API(twitter_search.auth, wait_on_rate_limit=True)
 
     query = session.get('query')
     if query is None:
         return redirect(url_for('index'))
 
-    search = Search(query)
-	potential_influencers = search.search_twitter()
+    search = twitter_search.Search(query)
+    potential_influencers = search.search_twitter()
 
     links = []
     for name in potential_influencers:
@@ -123,15 +123,15 @@ def oauth_authorized(response):
 
 @app.route('/about')
 def about():
-	data = {}
-	return render_template('about.html', data=data)
+    data = {}
+    return render_template('about.html', data=data)
 
 
 @app.route('/contact')
 def contact():
-	data = {}
-	return render_template('contact.html', data=data)
+    data = {}
+    return render_template('contact.html', data=data)
 
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
